8ecf9ca82007b6059fb59156018f02895e674255
Francis Bato <batofdb@gmail.com>
2015-10-21 23:11:25 -0700

    added search

--- BookClub/FriendViewController.m
+++ BookClub/FriendViewController.m
@@ -11,23 +11,26 @@
 #import "ReadersViewController.h"
 #import "ProfileViewController.h"
 
-@interface FriendViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface FriendViewController () <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (nonatomic) NSArray *friends;
+@property NSArray *filteredFriends;
 @property NSMutableArray *results;
 @property NSManagedObjectContext *moc;
-
+@property UISearchController *searchController;
+@property BOOL isFiltered;
 @end
 
 @implementation FriendViewController
 
 
 - (void)viewDidLoad {
-
-    
-
     [super viewDidLoad];
+    self.isFiltered = NO;
+    self.filteredFriends = [NSArray new];
+
     self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
 
     AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
@@ -39,9 +42,10 @@
 -(void)load{
 
     NSFetchRequest *request = [[NSFetchRequest alloc]initWithEntityName:@"Reader"];
-    //request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"readingList.count" ascending:NO selector:@selector(localizedStandardCompare:)]];
     request.predicate = [NSPredicate predicateWithFormat:@"isFriend = 1"];
-    self.friends = [[NSArray alloc]initWithArray:[self.moc executeFetchRequest:request error:nil]];
+    self.friends= [[NSArray alloc]initWithArray:[self.moc executeFetchRequest:request error:nil]];
+    //NSArray* sortedArray = [[NSArray alloc] initWithArray:[array sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"readingList.count" ascending:NO selector:@selector(localizedStandardCompare:)]]]];
+
 
     [self.tableView reloadData];
 }
@@ -54,13 +58,20 @@
 
 
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.friends.count;
+    if (self.isFiltered)
+        return self.filteredFriends.count;
+    else
+        return self.friends.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
 
-    cell.textLabel.text = [self.friends[indexPath.row] name];
+    if (self.isFiltered) {
+        cell.textLabel.text = [self.filteredFriends[indexPath.row] name];
+    } else {
+        cell.textLabel.text = [self.friends[indexPath.row] name];
+    }
 
     return cell;
 }
@@ -80,6 +91,25 @@
     }
 }
 
+#pragma mark - search bar methods
+
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+{
+    NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"name contains[cd] %@", searchText];
+    self.filteredFriends = [self.friends filteredArrayUsingPredicate:resultPredicate];
+    [self.tableView reloadData];
+}
+
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+
+    self.isFiltered = NO;
+    if (searchText.length > 0)
+        self.isFiltered =YES;
+
+    NSLog(@"search: %@", searchText);
+    [self filterContentForSearchText:searchText scope:nil];
+}
 
 
 
